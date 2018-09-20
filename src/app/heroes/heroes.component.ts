ac38abe541fabde4007cf95737c2bd21d1298658
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-09-20 22:41:38 +0800

    remove some blank lines

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,8 +7,6 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
-
 export class HeroesComponent implements OnInit {
 
   // hero = 'Windstorm';
