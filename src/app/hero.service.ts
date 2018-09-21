4ed3e2e190b6fd21b0164b95a07805227e45192c
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-09-21 11:23:38 +0800

    let HeroService getHeroes became observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,8 @@ export class HeroService {
 
   }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 
 }
