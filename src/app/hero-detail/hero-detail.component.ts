e77cc1935ed33cbd8b81fd0d874abd805060c379
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-09-21 23:01:43 +0800

    add hero-detail routing codes

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -11,9 +14,24 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero: Hero;
 
-  constructor() { }
+  constructor(
+    private rt: ActivatedRoute,
+    private hs: HeroService,
+    private loc: Location
+  ) { }
 
   ngOnInit() {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.rt.snapshot.paramMap.get('id');
+    this.hs.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.loc.back();
   }
 
 }
