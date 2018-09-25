609741fad2f9fd8d9a5334df614afba403c0e0c1
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-09-25 11:40:39 +0800

    add a new hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -54,12 +54,18 @@ export class HeroService {
   }
 
   updateHero(hero: Hero): Observable<any> {
-
     return this.http.put(this.heroesUrl, hero, httpOptions)
       .pipe(
         tap(_ => this.log(`update hero id=${hero.id}`)),
         catchError(this.handleError<any>('updateHero'))
       )
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
   }
 
 }
