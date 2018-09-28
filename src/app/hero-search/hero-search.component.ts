0a049ecaad18f9618f79fe27bd07d77560e87ea3
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-09-28 11:46:58 +0800

    add actual search function

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable, Subject } from 'rxjs';
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+
 @Component({
   selector: 'app-hero-search',
   templateUrl: './hero-search.component.html',
@@ -7,9 +14,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroSearchComponent implements OnInit {
 
-  constructor() { }
+  heroes$: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private hs: HeroService) {
+  }
 
   ngOnInit() {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.hs.searchHeroes(term))
+    );
+  }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 
 }
