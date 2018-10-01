6193f6fdebfe50f1a50f657010c8e5cee342427e
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-10-01 08:47:38 +0800

    fix some bugs by tslint

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,7 +20,7 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes)
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   add(name: string): void {
@@ -33,7 +33,7 @@ export class HeroesComponent implements OnInit {
   }
 
   delete(hero: Hero): void {
-    this.heroes = this.heroes.filter(h => h != hero);
+    this.heroes = this.heroes.filter(h => h !== hero);
     this.heroService.deleteHero(hero).subscribe();
   }
 
